0b4282fd94ee180fb38c19ce5dfe96d2554021f4
notmeisit <145487489+notmeisit@users.noreply.github.com>
2023-10-11 17:11:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/Hamas_icon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>غزة تحت القصف-مناشدة عاجلة لإغاثة أهلنا في غزة</title>
+</head>
+
+<body>
+<nav class="top-nav">
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#videos">Videos</a></li>
+        <li><a href="#images">Images</a></li>
+        <li><a href="#donates">Donates</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+</nav>
+<div class="main">
+    <div id="home" class="top-image">
+        <img src="images/Hamas_logo.png" alt="Hamas logo" id="hamas-logo">
+    </div>
+
+    <div id="about" class="text-box">
+        <p class="p-black">غزة تحترق، حان دورنا لنتكاتف معاً ونكون عوناً وسنداً لإخوتنا في فلسطين.
+            <br><br>
+            لا يمكننا أن نكون معهم ولكن يمكننا أن نرسل مساعداتنا إليهم.
+        </p>
+        <p class="p-blue">
+            ★الأوضاع تتجه إلى تصعيد غير مسبوق، وتخوفات من إحتمالية تدهور الوضع في الساعات المقبلة:
+        </p>
+        <p class="p-black">
+            في قطاع غزة، بيّنت آخر حصيلة أوردتها وزارة الصحة، أن عدوان الاحتلال أسفر عن 232 شهيداً على الأقل وأكثر
+            من 1700 جريح من بينهم نساء وأطفال، فيما يتواصل قصف المباني المدنية والمنشآت والمنظمات الخيرية والمقار
+            التابعة لفصائل المقاومة الفلسطينية.
+            <br><br>
+            في ظل التطورات الحالية والهجمات العنيفة التي تشهدها قطاع غزة، نشهد أزمة إنسانية طارئة. المدنيين الأبرياء
+            المحاصرين تحت القصف يحتاجون بشدة إلى مساعدتنا. إننا نواجه مشكلة إنسانية خطيرة تتطلب تدخلًا عاجلًا.
+            <br><br>
+            هذه الأحداث تضع الأسر في مواجهة وضع صعب للغاية. منازلهم وممتلكاتهم تم تدميرها، وهم يعانون من نقص
+            الإمدادات الأساسية مثل الغذاء والدواء والمياه والكهرباء.
+        </p>
+        <p class="p-blue">
+            تبرعاتكم تنقذ حياة آلاف الأسر في غزة وفلسطين.
+            <br><br>
+            ★ما نحتاجه:
+        </p>
+        <p class="p-black">
+            نحتاج إلى توفير الغذاء والدواء والمياه والمستلزمات الطبية للمحاصرين في غزة وللجرحى الذين يعانون من آثار
+            الهجمات العنيفة.
+            <br><br>
+            اجعلوا تبرعاتكم تعبر الحدود وتصل الى أيدي محتاجيها.
+        </p>
+        <p class="p-blue">
+            معاً نضمد جراحهم ونعزز صمودهم،
+        </p>
+        <p class="p-black">
+            عن أبي موسى رضي الله عنه قال‏:‏ قال رسول الله صلى الله عليه وسلم ‏ "‏ المؤمن للمؤمن كالبنيان يشد بعضه
+            بعضًا‏"‏ وشبك بين أصابعه ‏.‏ ‏(‏‏(‏متفق عليه‏)‏‏)‏.
+        </p>
+    </div>
+
+    <div class="links-section">
+        <h2>تبرع الان</h2>
+        <div class="links-container">
+            <div class="link">
+                <button onclick="window.open('https://google.com','_blank')">التبرع بالتشفير</button>
+            </div>
+            <div class="link">
+                <button onclick="window.open('https://google.com','_blank')">التبرع الائتماني</button>
+            </div>
+        </div>
+    </div>
+
+    <br><br>
+
+    <div id="videos" class="videos">
+        <div class="video">
+            <video src="videos/gaza-1.mp4" controls></video>
+        </div>
+        <div class="video">
+            <video src="videos/gaza-2.mp4" controls></video>
+        </div>
+        <div class="video">
+            <video src="videos/gaza-3.mp4" controls></video>
+        </div>
+        <div class="video">
+            <video src="videos/gaza-4.mp4" controls></video>
+        </div>
+        <div class="video">
+            <video src="videos/gaza-5.mp4" controls></video>
+        </div>
+        <div class="video">
+            <video src="videos/gaza-6.mp4" controls></video>
+        </div>
+    </div>
+
+    <div id="images" class="stand-alone-images">
+        <div class="st-img-con">
+            <figure>
+                <img src="images/stand-alone/babies.jpg" alt="babies">
+                <!-- <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption> -->
+            </figure>
+        </div>
+        <div class="st-img-con">
+            <figure>
+                <img src="images/stand-alone/emergency.jpg" alt="emergency">
+                <figcaption>قامت وحدة الشرطة السيبرانية في لهب 433 بتجميد حسابات العملات المشفرة التي تستخدمها حماس
+                    لجمع التبرعات على الشبكات الاجتماعية.
+                    <br><br>
+                    في الصورة: إعلان لجمع التبرعات لحساب مجمد
+                </figcaption>
+            </figure>
+        </div>
+        <div class="st-img-con">
+            <figure>
+                <img src="images/stand-alone/dark.jpg" alt="dark">
+                <figcaption>شركة الاتصالات الفلسطينية بالتل في غزة: نتيجة للأضرار الجسيمة التي لحقت ببنيتنا التحتية،
+                    هناك انقطاعات وتباطؤ في خدمات الإنترنت السلكية والمحمولة. وتم تدمير المكونات المركزية للشبكة
+                    بالإضافة إلى الوصلات الدولية خارج غزة.
+                    وفي حال تضررت الروابط الدولية التي لا تزال تعمل، فسيتم قطع الإنترنت بشكل كامل عن غزة.
+                    <br><br>
+
+                    ونتيجة لذلك، قد لا يكون بعض الموظفين متاحين في المستقبل القريب
+                </figcaption>
+            </figure>
+        </div>
+    </div>
+
+    <div id="donates" class="links-section">
+        <h2>تبرع الان</h2>
+        <div class="links-container">
+            <div class="link">
+                <button onclick="window.open('https://google.com','_blank')">التبرع بالتشفير</button>
+            </div>
+            <div class="link">
+                <button onclick="window.open('https://google.com','_blank')">التبرع الائتماني</button>
+            </div>
+        </div>
+        <div id=contact class="contact">
+            <h3>معلومات الاتصال</h3>
+            <div class="contact-icons-container">
+                <div class="contact-icon">
+                    <a href="https://t.me/HamasSupport" target="_blank">
+                        <img src="images/icons/telegram-icon.png" alt="">
+                    </a>
+                </div>
+                <div class="contact-icon">
+                    <a href="https://www.instagram.com/support_hamas/" target="_blank">
+                        <img src="images/icons/instagram-icon.png" alt="">
+                    </a>
+                </div>
+
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
